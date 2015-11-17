4ee9206f70a9be9c8565f3ed3560c0742259d710
Seungyeon Kim <acuros@kaist.ac.kr>
2015-11-17 09:09:58 +0900

    Add Spinner actions/store

---
+++ src/stores/SpinnerStore.js
@@ -0,0 +1,28 @@
+var Reflux = require('reflux');
+
+var SpinnerActions = require('actions/SpinnerActions');
+
+
+var options = {
+    isVisible: false,
+    message: ""
+};
+
+module.exports = Reflux.createStore({
+    listenables: [SpinnerActions],
+    getInitialState: function() {
+        return options;
+    },
+
+    onShow: function(message) {
+        options = {
+            isVisible: true,
+            message: message
+        };
+        this.trigger(options)
+    },
+    onHide: function() {
+        options.isVisible = false;
+        this.trigger(options);
+    }
+});
