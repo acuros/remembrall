b07bc54bd4981835e4df16c8f1c0ef76d843c847
Seungyeon Kim <acuros@kaist.ac.kr>
2015-11-17 21:13:14 +0900

    Word.prototype.toItemOfUser

--- src/stores/Word.js
+++ src/stores/Word.js
@@ -7,5 +7,19 @@ Word.fromItem = function(item) {
     return new Word(item['word']['S'], item['meaning']['S']);
 };
 
+Word.prototype.toItemOfUser = function(userId) {
+    return {
+        user: {
+            S: userId
+        },
+        word: {
+            S: this.name
+        },
+        meaning: {
+            S: this.meaning
+        }
+    }
+};
+
 
 module.exports = Word;
