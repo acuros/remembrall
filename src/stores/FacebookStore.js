acae97f905974de6372905657d87d5377942a197
Seungyeon Kim <acuros@kaist.ac.kr>
2015-11-17 11:19:29 +0900

    Prepare dynamodb on fb authenticate succeed

--- src/stores/FacebookStore.js
+++ src/stores/FacebookStore.js
@@ -1,38 +1,54 @@
 var Reflux = require('reflux');
 
 var FacebookActions = require('actions/FacebookActions');
+var AWS = require('utils/aws');
 
 
-var fbData = {
+var data = {
     isAuthenticated: undefined,
     status: '',
-    userId: null
+    userId: null,
+    accessToken: null
 };
+
+function updateFromResponse(response) {
+    data.status = response.status;
+    data.isAuthenticated = data.status == 'connected';
+    if(data.isAuthenticated) {
+        data.userId = response.authResponse.userID;
+        data.accessToken = response.authResponse.accessToken;
+    }
+    FacebookStore.trigger(data);
+}
+
+function handleLogin(response) {
+    updateFromResponse(response);
+    if(data.isAuthenticated) {
+        AWS.prepareDynamodb(data.userId, data.accessToken).then(function() {
+            console.log(AWS, AWS.dynamodb);
+        }, function(err) {
+            console.log(err);
+        });
+    }
+}
 
 var FacebookStore = Reflux.createStore({
     listenables: [FacebookActions],
     getInitialState: function() {
-        return fbData;
+        return data;
     },
-    updateFromResponse: function(response) {
-        fbData.status = response.status;
-        fbData.isAuthenticated = fbData.status == 'connected';
-        if(fbData.isAuthenticated) {
-            fbData.userId = response.authResponse.userID;
-        }
-        this.trigger(fbData);
-    },
+
 
     onCheckLoginStatus: function() {
         FB.getLoginStatus(function(response) {
             FacebookActions.checkLoginStatus.completed();
-            this.updateFromResponse(response);
+            handleLogin(response);
         }.bind(this));
     },
     onLogin: function() {
         FB.login(function(response) {
             FacebookActions.login.completed();
-            this.updateFromResponse(response);
+            handleLogin(response);
         }.bind(this));
     }
 });
