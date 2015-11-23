091129303b1c951d2a81e2aae08ca6406e46abd8
Acuros <acuros@kaist.ac.kr>
2015-11-23 19:38:16 +0900

    Fetch wordlist, not words on login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ var Reflux = require('reflux');
 var Q = require('q');
 var Link = require('react-router').Link;
 
-var WordActions = require('actions/WordActions');
+var WordListActions = require('actions/WordListActions');
 var FacebookActions = require('actions/FacebookActions');
 var SpinnerActions = require('actions/SpinnerActions');
 
@@ -14,94 +14,91 @@ var AwsHelper = require('utils/AwsHelper');
 
 var Spinner = require('components/Spinner');
 var WordTester = require('components/WordTester');
-var WordStore = require('stores/WordStore');
 require('styles/style');
 
 var App = React.createClass({
-    mixins: [
-        Reflux.connect(WordStore, 'words'),
-        Reflux.connect(FacebookStore, 'facebook'),
-        Reflux.connect(SpinnerStore, 'spinner')
-    ],
+  mixins: [
+    Reflux.connect(FacebookStore, 'facebook'),
+    Reflux.connect(SpinnerStore, 'spinner')
+  ],
 
-    componentDidMount: function() {
-        SpinnerActions.show("Checking login status ...");
-        this.loadFacebookSDK(function() {
-            this.initWordsAfter(FacebookActions.checkLoginStatus);
-        }.bind(this));
-    },
-    render: function() {
-        return (
-            <div id="wrap" ref="wrap">
-                <header>
-                    <h1>Remembrall - Memorize and manage words conveniently</h1>
-                    {
-                        this.state.facebook.isAuthenticated === false &&
-                        <button id="facebook-login" onClick={this.loginWithFacebook}>Log In with Facebook</button>
-                    }
-                    {
-                        this.state.facebook.isAuthenticated === true &&
-                        <nav>
-                            <Link to="/">Test</Link> / <Link to="/manage">Manage</Link>
-                        </nav>
-                    }
-                </header>
-                {
-                    this.state.facebook.isAuthenticated === true &&
-                    (
-                        this.props.children &&
-                        <div id="children-wrap">{this.props.children}</div> ||
-                        <WordTester words={this.state.words} />
-                    )
-                }
-                {
-                    this.state.spinner.isVisible &&
-                    <Spinner message={this.state.spinner.message} />
-                }
-            </div>
-        );
-    },
-    loginWithFacebook: function() {
-        SpinnerActions.show("Logging in...");
-        this.initWordsAfter(FacebookActions.login);
-    },
-    initWordsAfter: function(action) {
-        action.triggerAsync()
-            .then(function(facebook) {
-                return Q.Promise(function(resolve, reject) {
-                    if(facebook.isAuthenticated)
-                        resolve();
-                    else
-                        reject('Facebook', 'Not authenticated');
-            })
-            .then(function() {
-                SpinnerActions.show("Acquiring permission ...");
-                return AwsHelper.prepareDynamodb(facebook.userId, facebook.accessToken)
-            })
-            .then(function() {
-                SpinnerActions.show("Getting words ...");
-                return WordActions.fetchWords.triggerAsync()
-            })
-            .catch(function(errType, msg) {
-                console.log(errType, msg);
-            })
-            .done(function() {
-                SpinnerActions.hide();
-            })
-        })
-    },
-    loadFacebookSDK: function(onFbAsyncInit) {
-        window.fbAsyncInit = function() {
-            onFbAsyncInit();
-        };
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=" + FB_APP_ID;
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    }
+  componentDidMount: function () {
+    SpinnerActions.show("Checking login status ...");
+    this.loadFacebookSDK(function () {
+      this.initWordsAfter(FacebookActions.checkLoginStatus);
+    }.bind(this));
+  },
+  render: function () {
+    return (
+      <div id="wrap" ref="wrap">
+        <header>
+          <h1>Remembrall - Memorize and manage words conveniently</h1>
+          {
+            this.state.facebook.isAuthenticated === false &&
+            <button id="facebook-login" onClick={this.loginWithFacebook}>Log In with Facebook</button>
+          }
+          {
+            this.state.facebook.isAuthenticated === true &&
+            <nav>
+              <Link to="/">Test</Link> / <Link to="/manage">Manage</Link>
+            </nav>
+          }
+        </header>
+        {
+          this.state.facebook.isAuthenticated === true &&
+          this.props.children &&
+          <div id="children-wrap">{this.props.children}</div>
+        }
+        {
+          this.state.spinner.isVisible &&
+          <Spinner message={this.state.spinner.message}/>
+        }
+      </div>
+    );
+  },
+  loginWithFacebook: function () {
+    SpinnerActions.show("Logging in...");
+    this.initWordsAfter(FacebookActions.login);
+  },
+  initWordsAfter: function (action) {
+    action.triggerAsync()
+      .then(function (facebook) {
+        return Q.Promise(function (resolve, reject) {
+          if (facebook.isAuthenticated)
+            resolve();
+          else
+            reject('Facebook', 'Not authenticated');
+      })
+      .then(function () {
+        SpinnerActions.show("Acquiring permission ...");
+        return AwsHelper.prepareDynamodb(facebook.userId, facebook.accessToken)
+      })
+      .then(function () {
+        SpinnerActions.show("Getting words ...");
+        return WordListActions.fetchWordLists.triggerAsync()
+      })
+      .catch(function (errType, msg) {
+        console.log(errType, msg);
+      })
+      .done(function () {
+        SpinnerActions.hide();
+        location.hash = "#/test";
+      })
+    })
+  },
+  loadFacebookSDK: function (onFbAsyncInit) {
+    window.fbAsyncInit = function () {
+      onFbAsyncInit();
+    };
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=" + FB_APP_ID;
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
 });
 
 
--- src/components/WordTester.jsx
+++ src/components/WordTester.jsx
@@ -2,90 +2,111 @@ var React = require('react');
 var Reflux = require('reflux');
 
 var WordCard = require('components/WordCard');
+var WordListStore = require('stores/WordListStore');
 
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex ;
-
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
 
 var WordTester = React.createClass({
-    nextCycleWords: [],
+  mixins: [Reflux.connect(WordListStore, 'wordLists')],
 
-    propTypes: {words: React.PropTypes.array.isRequired},
+  nextCycleWords: [],
 
-    getInitialState: function() {
-        return {
-            words: this.props.words,
-            wordIndex: 0,
-            corrects: 0
-        }
-    },
-    componentWillReceiveProps: function(nextProps) {
-        this.startNewCycle(nextProps.words);
-    },
-
-    render: function() {
-        return (
-            <section id="test-section">
-                <header>
-                    {Math.min(this.state.wordIndex + 1, this.state.words.length)} / {this.state.words.length}
-                </header>
-                {
-                    this.state.words.length > 0 &&
-                    (
-                        this.state.wordIndex < this.state.words.length &&
-                        <WordCard word={this.state.words[this.state.wordIndex]} onMark={this.markCurrentWord}/> ||
-                        this.createScoreBoard()
-                    )
-                }
-            </section>
-        )
-    },
-
-    createScoreBoard: function() {
-        return (
-            <div id="score-board">
-                <span className="score">Your score is {this.state.corrects} / {this.state.words.length}</span>
-                <div id="actions">
-                    <button className="finish-action" onClick={this.continueWithTheWrongs}>Continue</button>
-                    <button className="finish-action" onClick={this.restart}>Restart</button>
-                </div>
-            </div>
-        );
-    },
-    markCurrentWord: function(isCorrect) {
-        if(!isCorrect) {
-            this.nextCycleWords.push(this.state.words[this.state.wordIndex]);
-        }
-        this.setState({
-            wordIndex: this.state.wordIndex + 1,
-            corrects: this.state.corrects + (isCorrect ? 1 : 0)
-        });
-    },
-    startNewCycle: function(words) {
-        this.nextCycleWords = [];
-        this.setState({
-            words: shuffle(words),
-            wordIndex: 0,
-            corrects: 0
-        });
-    },
-    restart: function() {
-        this.startNewCycle(this.props.words);
-    },
-    continueWithTheWrongs: function() {
-        this.startNewCycle(this.nextCycleWords);
+  getInitialState: function () {
+    return {
+      words: [],
+      wordIndex: 0,
+      corrects: 0
     }
+  },
+
+  render: function () {
+    return this.createWordList();
+  },
+
+  createWordList: function () {
+    var lis = this.state.wordLists.map(function (wordList, index) {
+      var liKey = "wordlist-" + index + "-li";
+      var checkboxId = "wordlist-" + index + "-checkbox";
+      return (
+        <li key={liKey}>
+          <input type="checkbox" name="wordlist" id={checkboxId}/>
+          <label htmlFor={checkboxId}>{wordList}</label>
+        </li>
+      );
+    });
+    return (
+      <form method="get">
+        <ul>{lis}</ul>
+        <button>Start test</button>
+      </form>
+    );
+  },
+  createTestBoard: function () {
+    return (
+      <section id="test-section">
+        <header>
+          {Math.min(this.state.wordIndex + 1, this.state.words.length)} / {this.state.words.length}
+        </header>
+        {
+          this.state.words.length > 0 &&
+          (
+            this.state.wordIndex < this.state.words.length &&
+            <WordCard word={this.state.words[this.state.wordIndex]} onMark={this.markCurrentWord}/> ||
+            this.createScoreBoard()
+          )
+        }
+      </section>
+    )
+  },
+  createScoreBoard: function () {
+    return (
+      <div id="score-board">
+        <span className="score">Your score is {this.state.corrects} / {this.state.words.length}</span>
+
+        <div id="actions">
+          <button className="finish-action" onClick={this.continueWithTheWrongs}>Continue</button>
+          <button className="finish-action" onClick={this.restart}>Restart</button>
+        </div>
+      </div>
+    );
+  },
+  markCurrentWord: function (isCorrect) {
+    if (!isCorrect) {
+      this.nextCycleWords.push(this.state.words[this.state.wordIndex]);
+    }
+    this.setState({
+      wordIndex: this.state.wordIndex + 1,
+      corrects: this.state.corrects + (isCorrect ? 1 : 0)
+    });
+  },
+  startNewCycle: function (words) {
+    this.nextCycleWords = [];
+    this.setState({
+      words: shuffle(words),
+      wordIndex: 0,
+      corrects: 0
+    });
+  },
+  restart: function () {
+    this.startNewCycle(this.props.words);
+  },
+  continueWithTheWrongs: function () {
+    this.startNewCycle(this.nextCycleWords);
+  }
 });
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 module.exports = WordTester;
