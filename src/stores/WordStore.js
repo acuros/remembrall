f0b31a6f1290ae9a83317c37581192b21d478c75
Acuros <acuros@kaist.ac.kr>
2015-11-23 15:55:20 +0900

    Encapsulate dynamodb

--- src/stores/WordStore.js
+++ src/stores/WordStore.js
@@ -6,53 +6,41 @@ var WordActions = require('actions/WordActions');
 var AwsHelper = require('utils/AwsHelper');
 
 
-var TABLE_NAME = 'Word';
 var words = [];
 
-
 var WordStore = Reflux.createStore({
-    listenables: [WordActions],
-    getInitialState: function() {
-        return []
-    },
-    getList: function() {
-        return words.slice();
-    },
+  listenables: [WordActions],
+  getInitialState: function () {
+    return []
+  },
+  getList: function () {
+    return words.slice();
+  },
 
-    onFetchWords: function() {
-        AwsHelper.dynamodb.query({
-            TableName: TABLE_NAME,
-            KeyConditions: {
-                user: {
-                    ComparisonOperator: 'EQ',
-                    AttributeValueList: [{S: FacebookStore.getState().userId}]
-                }
-            }
-        }, function(err, data) {
-            if(err) {
-                WordActions.fetchWords.failed('Dynamodb', err);
-                return;
-            }
-            WordActions.fetchWords.completed();
-            words = data.Items.map(function(item) {return Word.fromItem(item)});
-            WordStore.trigger(words);
-        });
-    },
-    onUploadWord: function(word) {
-        AwsHelper.dynamodb.putItem({
-            TableName: TABLE_NAME,
-            Item: word.toItemOfUser(FacebookStore.getState().userId)
-        },
-        function(err, data) {
-            if(err) {
-                WordActions.uploadWord.failed('Dynamodb', err);
-                return;
-            }
-            WordActions.uploadWord.completed();
-            words.push(word);
-            WordStore.trigger(words);
-        });
-    }
+  onFetchWords: function () {
+    AwsHelper.fetchWords(function (err, data) {
+      if (err) {
+        WordActions.fetchWords.failed('Dynamodb', err);
+        return;
+      }
+      WordActions.fetchWords.completed();
+      words = data.Items.map(function (item) {
+        return Word.fromItem(item)
+      });
+      WordStore.trigger(words);
+    });
+  },
+  onUploadWord: function (word) {
+    AwsHelper.putWord(word, function (err, data) {
+      if (err) {
+        WordActions.uploadWord.failed('Dynamodb', err);
+        return;
+      }
+      WordActions.uploadWord.completed();
+      words.push(word);
+      WordStore.trigger(words);
+    });
+  }
 });
 
 module.exports = WordStore;
--- src/utils/AwsHelper.js
+++ src/utils/AwsHelper.js
@@ -1,41 +1,70 @@
 var Q = require('q');
 
-function makeAwsCredential(credentialData) {
-    return new AWS.Credentials(
-        credentialData.AccessKeyId,
-        credentialData.SecretAccessKey,
-        credentialData.SessionToken
-    );
-}
+var FacebookStore = require('stores/FacebookStore');
+
+
+var dynamodb = null;
+var WORD_TABLE_NAME = 'Word';
 
 
 var AwsHelper = {
-    dynamodb: null,
-    prepareDynamodb: function(userId, accessToken) {
-        return Q.Promise(function(resolve, reject) {
-            new AWS.STS().assumeRoleWithWebIdentity(
-                {
-                    RoleArn: ROLE_ARN,
-                    RoleSessionName: userId,
-                    WebIdentityToken: accessToken,
-                    ProviderId: 'graph.facebook.com'
-                },
-                function(err, data) {
-                    if(err) {
-                        reject('STS', err);
-                    }
-                    else {
-                        var credential = makeAwsCredential(data.Credentials);
-                        AwsHelper.dynamodb = new AWS.DynamoDB({
-                            credentials: credential,
-                            region: 'ap-northeast-1'
-                        });
-                        resolve();
-                    }
-                }
-            );
-        });
-    }
+  prepareDynamodb: function (userId, accessToken) {
+    return Q.Promise(function (resolve, reject) {
+      new AWS.STS().assumeRoleWithWebIdentity(
+        {
+          RoleArn: ROLE_ARN,
+          RoleSessionName: userId,
+          WebIdentityToken: accessToken,
+          ProviderId: 'graph.facebook.com'
+        },
+        function (err, data) {
+          if (err) {
+            reject('STS', err);
+          }
+          else {
+            var credential = makeAwsCredential(data.Credentials);
+            dynamodb = new AWS.DynamoDB({
+              credentials: credential,
+              region: 'ap-northeast-1'
+            });
+            resolve();
+          }
+        }
+      );
+    });
+  },
+
+  fetchWords: function(callback) {
+    callback = callback || function(){};
+    dynamodb.query({
+      TableName: WORD_TABLE_NAME,
+      KeyConditions: makeUserKeyCondition()
+    }, callback);
+  },
+  putWord: function(word, callback) {
+    callback = callback || function(){};
+    dynamodb.putItem({
+      TableName: WORD_TABLE_NAME,
+      Item: word.toItemOfUser(FacebookStore.getState().userId)
+    }, callback);
+  }
 };
+
+function makeAwsCredential(credentialData) {
+  return new AWS.Credentials(
+    credentialData.AccessKeyId,
+    credentialData.SecretAccessKey,
+    credentialData.SessionToken
+  );
+}
+
+function makeUserKeyCondition()  {
+  return {
+    user: {
+      ComparisonOperator: 'EQ',
+      AttributeValueList: [{S: FacebookStore.getState().userId}]
+    }
+  };
+}
 
 module.exports = AwsHelper;
